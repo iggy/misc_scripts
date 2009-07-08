5f9705fd2c4d08e0eaaea3ba4c65fed622b9d1f9
Brian Jackson <iggy@theiggy.com>
2009-07-08 14:45:57 -0500

    added progress meter to pydnbshare

--- pydnbshare.py
+++ pydnbshare.py
@@ -3,16 +3,13 @@
 
 # script to download releases from the dnbshare feed
 
-# TODO it currently reads the entire mp3 into memory instead of writing
-# it out to disk as it gets it
-# what happens if the script dies?
-# progress meter?
+# TODO 
+# what happens if the script dies? loses connection? whatever?
 
 URL="http://www.dnbshare.com/feed"
 
-import urllib2, re, httplib, urllib, os.path
+import urllib2, re, httplib, urllib, os.path, sys, math
 from xml.dom.minidom import parse
-from pprint import pprint
 
 fd = urllib2.urlopen(URL)
 
@@ -21,43 +18,48 @@ xml = parse(fd)
 links = xml.getElementsByTagName('link')[1:]
 
 for link in links:
-  url = link.firstChild.data
-  mp3 = url.replace('.html', '')
-  rslash = mp3.rfind('/') + 1
+    url = link.firstChild.data
+    mp3 = url.replace('.html', '')
+    rslash = mp3.rfind('/') + 1
 
-  if os.path.exists(mp3[rslash:]):
-    continue
-  
-  print url
-  pg = urllib2.urlopen(url).read()
-  
-  payload = re.search('name="payload" value="([^"]*)', pg).group(1)
-  
-  #data = 'file=%s&payload=%s' % (mp3[rslash:], payload)
-  data = urllib.urlencode({'file': mp3[rslash:], 'payload': payload})
-  
-  #print "posting to: ", url, data
-  
-  headers = {"Content-type": "application/x-www-form-urlencoded",'Referer': url}
-  
-  conn = httplib.HTTPConnection("dnbshare.com")
-  
-  conn.request('POST', url.replace('http://dnbshare.com',''), data, headers)
-  resp = conn.getresponse()
+    if os.path.exists(mp3[rslash:]):
+        continue
 
-  #pprint(resp.getheaders)
-  mp3url = resp.getheader('Location')
+    print url
+    pg = urllib2.urlopen(url).read()
 
-  conn.close()
+    payload = re.search('name="payload" value="([^"]*)', pg).group(1)
 
-  out = open(mp3[rslash:], 'w+')
-  mp3f = urllib2.urlopen(mp3url)
-  out.write(mp3f.read())
+    data = urllib.urlencode({'file': mp3[rslash:], 'payload': payload})
 
-#post = urllib2.urlopen(url, data)
-  
-  #print post.geturl()
-  #out = open('tmp', 'w+')
-  #out.write(post.read())
-  
-  #break
+    headers = {"Content-type": "application/x-www-form-urlencoded",'Referer': url}
+
+    conn = httplib.HTTPConnection("dnbshare.com")
+
+    conn.request('POST', url.replace('http://dnbshare.com',''), data, headers)
+    resp = conn.getresponse()
+
+    mp3url = resp.getheader('Location')
+
+    conn.close()
+
+    out = open(mp3[rslash:], 'w+')
+    mp3f = urllib2.urlopen(mp3url)
+    osize = float(mp3f.info().getheader('Content-Length'))
+
+    #print "Size: %10s", mp3f.info().getheader('Content-Length')
+
+    amountread = float(0)
+    #print "  0%"
+    while mp3f:
+        data = mp3f.read(4096)
+        if len(data) == 0:
+            break
+        amountread += len(data)
+        pct = amountread / osize * 100
+        sys.stdout.write("\r[T:%10d][R:%10d] %8d%%" % (osize, amountread, pct))
+        sys.stdout.flush()
+        out.write(data)
+
+    print "\n"
+    out.close()
